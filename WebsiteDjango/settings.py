0787b3af920482cceb51b39a6b38cb1ba98f0c04
Teemu <teemurahkonen@gmail.com>
2022-02-07 21:12:03 +0100

    docker stuff

--- WebsiteDjango/settings.py
+++ WebsiteDjango/settings.py
@@ -100,7 +100,7 @@ cloudinary.config(
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
+        'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': 'db',
